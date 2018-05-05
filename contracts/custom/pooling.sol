71eecc3db87c00422a95e74469ed472ca5962e18
Kirill <kk@bankexfoundation.org>
2018-05-06 01:36:14 +0300

    Add new test branch. Add some functions and ideas

--- contracts/custom/pooling.sol
+++ contracts/custom/pooling.sol
@@ -8,7 +8,7 @@ contract IPool is Ownable {
   address public owner;
   address public targetToken;
   uint public raisingTimeout;
-  uint public icoTimeout;
+  // uint public icoTimeout;
   uint public minimalDeposit;
   uint public minimalFundSize;
   uint public maximalFundSize;
@@ -16,7 +16,7 @@ contract IPool is Ownable {
   mapping (address => uint8) public approvedInvestors;
   function approveInvestor(address investor) public returns(bool);
   function () public payable;
-  function releaseInterest(uint value) public;
+  function releaseInterest(uint value) public retu;
   function moneyBack(uint value) public;
   function poolState() public view returns(uint8);
 }
@@ -24,7 +24,11 @@ contract IPool is Ownable {
 contract Pool is IPool {
     
   using SafeMath for uint256;
-  uint256 totalAccptedETH;
+
+  event MoneyBack(address indexed to, uint256 value);
+  event TransferToIcoManager(address indexed to, uint256 value);
+
+  uint256 totalAcceptedETH;
 
   mapping(address=>uint256) investorSum;
 
@@ -46,7 +50,7 @@ contract Pool is IPool {
   }
     
   modifier acceptedDeposit() {
-    require(msg.value <= minimalDeposit);
+    require(minimalDeposit <= msg.value);
     _;
   }
     
@@ -59,15 +63,33 @@ contract Pool is IPool {
     poolManager = msg.sender;
     icoManager = msg.sender;
     owner = msg.sender;
+    poolState = 0;
+    minimalFundSize = 100e18;
+    maximalFundSize = 300e18;
+    raisingTimeout = 1625556505;
+    icoTimeout = 1635556505;
+    fundDeprecatedTimeout = 1825556505;
   }
   
   function () public payable onlyApproved acceptedDeposit acceptedRaisingTimeout {
-    totalAccptedETH = totalAccptedETH.add(msg.value);
+    require(maximalFundSize >= totalAcceptedETH.add(msg.value));
+    totalAcceptedETH = totalAcceptedETH.add(msg.value);
     investorSum[msg.sender] = investorSum[msg.sender].add(msg.value);
   }
+
+  function moneyBack(uint _value) public returns(bool) {
+    require(poolState == 3);
+    require(investorSum[msg.sender] >= _value);
+    require(_value >= minimalDeposit);
+    investorSum[msg.sender] = investorSum[msg.sender].sub(_value);
+    totalAcceptedETH = totalAcceptedETH.sub(_value);
+    msg.sender.transfer(_value);
+    emit MoneyBack(msg.sender, _value);
+    return true;
+  }
     
-  function approveInvestor(address investor)  public onlyOwner returns(bool) {
-    approvedInvestors[investor] = 1;
+  function approveInvestor(address _investor)  public onlyOwner returns(bool) {
+    approvedInvestors[_investor] = 1;
     return true;
   }
 
@@ -80,16 +102,29 @@ contract Pool is IPool {
     }
   }
 
-  function getRaisingETH(uint256 value) public onlyIcoManager returns(bool) {
-    require(value >= totalAccptedETH);
-    icoManager.transfer(value);
+  function getRaisingETH(uint256 _value) public onlyIcoManager returns(bool) {
+    require(poolState == 2);  
+    require(totalAcceptedETH >= value);
+    totalAcceptedETH = totalAcceptedETH.sub(_value);
+    icoManager.transfer(_value);
+    emit TransferToIcoManager(icoManager, _value);
     return true;
+  }
+
+  function releaseInterest(uint value) public returns(bool) {
+    require(poolState == 4);
   }
 
   function poolState() public view returns(uint8) {
     return poolState;
   }
 
+  /** Можно сделать так, чтобы вручную нельзя было устанавливать poolState. 
+   *  Вызвать функцию может кто угодно. 
+   *  При вызове проверяется блок таймстемп и через if else различные даты, которые устанавливаются в конструкторе
+   *  Если подходит под дату, то устанавливается соответствующее состояние. 
+   *  Так же делаются проверки на минимальную сумму сбора после закрытия сбора средств.
+   */
   function setPoolState(uint8 _state) public returns(bool) {
     if(block.timestamp <= raisingTimeout && _state == 1) { 
       poolState = _state;
