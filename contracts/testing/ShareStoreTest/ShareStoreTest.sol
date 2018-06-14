84a65a6d478bb55aca68244100b10a23b034866b
Nikita Kozlov <nk@bankexfoundation.org>
2018-06-15 00:05:26 +0300

    make code more clear

--- contracts/testing/ShareStoreTest/ShareStoreTest.sol
+++ contracts/testing/ShareStoreTest/ShareStoreTest.sol
@@ -8,7 +8,7 @@ contract ShareStoreTest is ShareStore, TimeMachineT {
   uint8 internal role_;
   address internal roleAddress_;
   
-  uint256 public max_value_test = 2**256 -1;
+  uint256 public max_value_test = 2 ** 256 - 1;
 
 
   uint8 internal state_;
